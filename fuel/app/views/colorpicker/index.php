c1bf70e0d707120e74199c88c69c8d0f7a5d76c8
Sam McKay <sammckay@colostate.edu>
2023-03-27 16:36:49 -0600

    prep the input boxes for php get method

--- fuel/app/views/colorpicker/index.php
+++ fuel/app/views/colorpicker/index.php
@@ -3,8 +3,12 @@
 		<h3>HomePage</h3>
 	</header>
 	<main>
-        <input type="number"/>
-        <br/>
-        <input type="number"/>
+        <form name="rows form" action="" method="get">
+            <input type="number" name="rows" id="rows" value="rows"/>
+        <form>
+        </br>
+        <form name="columns form" action="" method="get">
+            <input type="number" name="columns" id="columns" value="columns"/>
+        </form>
     </main>
 </body>
