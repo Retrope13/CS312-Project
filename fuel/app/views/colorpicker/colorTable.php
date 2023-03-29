d7edf5e7e31c42c67de33fe22e17af3be1da1791
Sam McKay <sammckay@colostate.edu>
2023-03-28 19:14:57 -0600

    temp colorTable page

---
+++ fuel/app/views/colorpicker/colorTable.php
@@ -0,0 +1,17 @@
+<body>
+    <header>
+		<h3>Home Page</h3>
+	</header>
+	<main>
+        <form name="rows form" action="" method="get">
+            <input type="number" name="rows" id="rows" placeholder="# of rows/columns"value="rows"/>
+        <form>
+        </br>
+        <form name="columns form" action="" method="get">
+            <input type="number" name="colors" id="colors" placeholder="# of colors" value="colors"/>
+        </form>
+        <form action="table" method="POST">
+        <input id = "submit" type="submit"/>
+        </form>
+    </main>
+</body>
