ac4881e97c7dcb9b467bfe9ab0ae0a4606a6f8a5
Sam McKay <sammckay@colostate.edu>
2023-03-31 21:54:48 -0600

    ???

--- fuel/app/views/colorpicker/index.php
+++ fuel/app/views/colorpicker/index.php
@@ -4,14 +4,14 @@
 	</header>
 	<main>
         <?php echo Form::open(array('action' => 'index.php/colorpicker/table', 'method' => 'get')); ?>
-        
+
         <?php echo Form::label('Rows', 'Number of Rows:'); ?>
         <?php echo Form::input('rows', Input::get('rows'), array('placeholder' => 'Enter number of rows')); ?>
+
         <br>
         <?php echo Form::label('Colors', 'Number of Colors:'); ?>
         <?php echo Form::input('colors', Input::get('colors'), array('placeholder' => 'Enter number of colors')); ?>
         <?php echo Form::submit('submit', 'Submit'); ?>
-        
         <?php echo Form::close(); ?>
     </main>
 </body>
