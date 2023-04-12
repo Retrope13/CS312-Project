705b6a63b6293533344585e46ee52c81071f1114
Sam McKay <sammckay@colostate.edu>
2023-04-12 17:07:55 -0600

    get rid of forms in index

--- fuel/app/views/colorpicker/index.php
+++ fuel/app/views/colorpicker/index.php
@@ -1,17 +1,10 @@
 <body>
     <header>
-		<h3>Go ahead and choose dimensions for you table and the number of colors you want!</h3>
+		<h3>Our company The Color Garden hopes to bring you a boquet of colors and allow you to create your own garden. </h3>
+            <h3> By navigating to our color picker tab you can select the number of colors you want and the dimensions of your garden! </h3>
+            <h3> If you wish to know more about us then make your way to the about page!</h3>
 	</header>
 	<main>
-        <?php echo Form::open(array('action' => 'index.php/colorpicker/table', 'method' => 'get')); ?>
-        
-        <?php echo Form::label('Rows', 'Number of Rows:'); ?>
-        <?php echo Form::input('rows', Input::get('rows'), array('placeholder' => 'Enter number of rows')); ?>
-        <br>
-        <?php echo Form::label('Colors', 'Number of Colors:'); ?>
-        <?php echo Form::input('colors', Input::get('colors'), array('placeholder' => 'Enter number of colors')); ?>
-        <?php echo Form::submit('submit', 'Submit'); ?>
-        
-        <?php echo Form::close(); ?>
+
     </main>
 </body>
