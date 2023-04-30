12bd87ed41c4813f3beca218869cf0c7ef746eea
Sam McKay <sammckay@colostate.edu>
2023-04-30 01:13:20 -0600

    error logs

---
+++ fuel/app/logs/2023/04/29.php
@@ -0,0 +1,30 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2023-04-29 16:42:25 --> Notice - Undefined index: color3 in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/classes/controller/colorpicker.php on line 94
+ERROR - 2023-04-29 16:42:31 --> Notice - Undefined index: color3 in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/classes/controller/colorpicker.php on line 94
+ERROR - 2023-04-29 16:44:31 --> Notice - Undefined index: color0 in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/classes/controller/colorpicker.php on line 80
+ERROR - 2023-04-29 16:46:44 --> Notice - Undefined index: color3 in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/classes/controller/colorpicker.php on line 94
+ERROR - 2023-04-29 16:46:47 --> Notice - Undefined index: color3 in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/classes/controller/colorpicker.php on line 94
+ERROR - 2023-04-29 16:47:01 --> Notice - Undefined index: color4 in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/classes/controller/colorpicker.php on line 94
+ERROR - 2023-04-29 16:48:52 --> Notice - Undefined index: color in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/classes/controller/colorpicker.php on line 94
+ERROR - 2023-04-29 16:49:13 --> Notice - Undefined index: colors3 in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/classes/controller/colorpicker.php on line 94
+ERROR - 2023-04-29 16:51:43 --> Warning - A non-numeric value encountered in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/classes/controller/colorpicker.php on line 94
+ERROR - 2023-04-29 16:51:54 --> Notice - Undefined index: color2 in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/classes/controller/colorpicker.php on line 94
+ERROR - 2023-04-29 16:52:26 --> Notice - Undefined index: red in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/classes/controller/colorpicker.php on line 87
+ERROR - 2023-04-29 22:31:56 --> Warning - in_array() expects parameter 2 to be array, string given in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/classes/controller/colorpicker.php on line 82
+ERROR - 2023-04-29 22:33:18 --> Warning - in_array() expects parameter 2 to be array, integer given in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/classes/controller/colorpicker.php on line 82
+ERROR - 2023-04-29 22:33:31 --> Warning - in_array() expects parameter 2 to be array, string given in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/classes/controller/colorpicker.php on line 82
+ERROR - 2023-04-29 22:50:18 --> Warning - A non-numeric value encountered in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/classes/controller/colorpicker.php on line 79
+ERROR - 2023-04-29 23:26:00 --> Notice - Undefined offset: 2 in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/views/colorpicker/colorTable.php on line 35
+ERROR - 2023-04-29 23:28:17 --> Notice - Undefined variable: selected in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/views/colorpicker/colorTable.php on line 35
+ERROR - 2023-04-29 23:43:49 --> Notice - Undefined index: i in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/views/colorpicker/colorTable.php on line 48
+ERROR - 2023-04-29 23:49:45 --> Notice - Undefined offset: 5 in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/app/views/colorpicker/colorTable.php on line 51
+ERROR - 2023-04-29 23:58:02 --> shutdown - Fuel requires Sodium support in PHP. Either use PHP 7.2+, install the libsodium PECL extension, of the sodium-compat composer package! in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/core/vendor/paragonie.php on 33
+ERROR - 2023-04-29 23:58:05 --> shutdown - Fuel requires Sodium support in PHP. Either use PHP 7.2+, install the libsodium PECL extension, of the sodium-compat composer package! in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/core/vendor/paragonie.php on 33
+ERROR - 2023-04-29 23:58:05 --> shutdown - Fuel requires Sodium support in PHP. Either use PHP 7.2+, install the libsodium PECL extension, of the sodium-compat composer package! in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/core/vendor/paragonie.php on 33
+ERROR - 2023-04-29 23:58:07 --> shutdown - Fuel requires Sodium support in PHP. Either use PHP 7.2+, install the libsodium PECL extension, of the sodium-compat composer package! in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/core/vendor/paragonie.php on 33
+ERROR - 2023-04-29 23:58:26 --> shutdown - Fuel requires Sodium support in PHP. Either use PHP 7.2+, install the libsodium PECL extension, of the sodium-compat composer package! in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/core/vendor/paragonie.php on 33
+ERROR - 2023-04-29 23:58:26 --> shutdown - Fuel requires Sodium support in PHP. Either use PHP 7.2+, install the libsodium PECL extension, of the sodium-compat composer package! in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/core/vendor/paragonie.php on 33
+ERROR - 2023-04-29 23:59:21 --> shutdown - Fuel requires Sodium support in PHP. Either use PHP 7.2+, install the libsodium PECL extension, of the sodium-compat composer package! in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/core/vendor/paragonie.php on 33
+ERROR - 2023-04-29 23:59:23 --> shutdown - Fuel requires Sodium support in PHP. Either use PHP 7.2+, install the libsodium PECL extension, of the sodium-compat composer package! in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/core/vendor/paragonie.php on 33
+ERROR - 2023-04-29 23:59:23 --> shutdown - Fuel requires Sodium support in PHP. Either use PHP 7.2+, install the libsodium PECL extension, of the sodium-compat composer package! in /s/bach/n/under/sammckay/local_html/CS312-Project/fuel/core/vendor/paragonie.php on 33
