721bbd76d4c2e93910766613e88e4ec2b6479748
Sam McKay <sammckay@colostate.edu>
2023-05-03 22:32:50 -0600

    spell bouquet right

--- fuel/app/views/colorpicker/index.php
+++ fuel/app/views/colorpicker/index.php
@@ -1,6 +1,6 @@
 <body>
     <header>
-		<h3>Our company The Color Garden hopes to bring you a boquet of colors and allow you to create your own garden. </h3>
+		<h3>Our company The Color Garden hopes to bring you a bouquet of colors and allow you to create your own garden. </h3>
             <h3> By navigating to our color picker tab you can select the number of colors you want and the dimensions of your garden! </h3>
             <h3> If you wish to know more about us then make your way to the about page!</h3>
 	</header>
